28d5583bc9ca41e37ca142bd0e7c9092b88459f4
Cara Hill <cara@rabidtech.co.nz>
2018-10-28 20:49:15 +1300

    Refactored function.

--- reverse-string.js
+++ reverse-string.js
@@ -1,15 +1,8 @@
 function reverseString(string) {
-  if(string.length == 0) {
-    return string;
-  } else {
-    let splitString = string.split("");
-
-    let reverseArray = splitString.reverse();
-
-    let joinReversedArray = reverseArray.join("");
-
-    return joinReversedArray;
-  }
+  return string
+          .split("")
+          .reverse()
+          .join("");
 }
 
 export { reverseString }
